8c15e658c2c02e75f04059792b64506c96f2a3eb
Vittorio Esposito <esposito.vittorio@rocketmail.com>
2021-10-01 13:56:54 +0200

    create Taco.java

---
+++ src/main/java/com/example/tacos/model/Taco.java
@@ -0,0 +1,12 @@
+package com.example.tacos.model;
+
+import lombok.Data;
+import lombok.RequiredArgsConstructor;
+
+@Data
+@RequiredArgsConstructor
+public class Taco {
+
+    private final int id;
+
+}
