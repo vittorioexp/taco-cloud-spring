a984500212b9e3111ece42f9fc96141bdf0b0801
Vittorio Esposito <esposito.vittorio@rocketmail.com>
2021-10-01 14:36:48 +0200

    create order object

---
+++ src/main/java/com/example/tacos/model/Order.java
@@ -0,0 +1,21 @@
+package com.example.tacos.model;
+
+import lombok.Data;
+
+@Data
+public class Order {
+
+    private String name;
+    private String street;
+    private String city;
+    private String state;
+    private String zip;
+    private String ccNumber;
+    private String ccExpiration;
+    private String ccCVV;
+
+    public Order() {
+
+    }
+
+}
