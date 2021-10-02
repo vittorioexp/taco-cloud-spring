d99f21d4de5885850d3e9f04136967401560c8e8
Vittorio Esposito <esposito.vittorio@rocketmail.com>
2021-10-02 12:08:10 +0200

    fix processOrder method to validate input

--- src/main/java/com/example/tacos/controller/DesignTacoController.java
+++ src/main/java/com/example/tacos/controller/DesignTacoController.java
@@ -6,10 +6,12 @@ import com.example.tacos.model.Taco;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import javax.validation.Valid;
 import java.util.Arrays;
 import java.util.List;
 
@@ -57,7 +59,10 @@ public class DesignTacoController {
     }
 
     @PostMapping
-    public String processDesign(Design design) {
+    public String processDesign(@Valid Taco design, Errors errors) {
+        if (errors.hasErrors()) {
+            return "design";
+        }
         // TODO: Save the taco design
         log.info("Processing design: " + design);
         return "redirect:/orders/current";
--- src/main/java/com/example/tacos/controller/OrderController.java
+++ src/main/java/com/example/tacos/controller/OrderController.java
@@ -5,9 +5,12 @@ import com.example.tacos.model.*;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.validation.Valid;
 
 @Slf4j
 @Controller
@@ -21,8 +24,12 @@ public class OrderController {
     }
 
     @PostMapping
-    public String processOrder(PurchaseOrder tacoOrder) {
-        log.info("Order submitted: " + tacoOrder);
+    public String processOrder(@Valid PurchaseOrder order, Errors errors) {
+        if (errors.hasErrors()) {
+            return "orderForm";
+        }
+        log.info("Order submitted: " + order);
         return "redirect:/";
     }
+
 }
