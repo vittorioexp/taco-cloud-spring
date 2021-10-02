5d6519d8018ff597ac74f1da736d2724486e9823
Vittorio Esposito <esposito.vittorio@rocketmail.com>
2021-10-02 11:40:06 +0200

    fix class

--- src/main/java/com/example/tacos/controller/DesignTacoController.java
+++ src/main/java/com/example/tacos/controller/DesignTacoController.java
@@ -35,16 +35,16 @@ public class DesignTacoController {
 
         // TODO: remove hard coded list of ingredients
         List<Ingredient> ingredients = Arrays.asList(
-                new Ingredient(1, "Flour Tortilla", Ingredient.Type.WRAP),
-                new Ingredient(2, "Corn Tortilla", Ingredient.Type.WRAP),
-                new Ingredient(3, "Ground Beef", Ingredient.Type.PROTEIN),
-                new Ingredient(4, "Carnitas", Ingredient.Type.PROTEIN),
-                new Ingredient(5, "Diced Tomatoes", Ingredient.Type.VEGGIES),
-                new Ingredient(6, "Lettuce", Ingredient.Type.VEGGIES),
-                new Ingredient(7, "Cheddar", Ingredient.Type.CHEESE),
-                new Ingredient(8, "Monterrey Jack", Ingredient.Type.CHEESE),
-                new Ingredient(9, "Salsa", Ingredient.Type.SAUCE),
-                new Ingredient(10, "Sour Cream", Ingredient.Type.SAUCE)
+                new Ingredient("FLTO", "Flour Tortilla", Ingredient.Type.WRAP),
+                new Ingredient("COTO", "Corn Tortilla", Ingredient.Type.WRAP),
+                new Ingredient("GRBF", "Ground Beef", Ingredient.Type.PROTEIN),
+                new Ingredient("CARN", "Carnitas", Ingredient.Type.PROTEIN),
+                new Ingredient("TMTO", "Diced Tomatoes", Ingredient.Type.VEGGIES),
+                new Ingredient("LETC", "Lettuce", Ingredient.Type.VEGGIES),
+                new Ingredient("CHED", "Cheddar", Ingredient.Type.CHEESE),
+                new Ingredient("JACK", "Monterrey Jack", Ingredient.Type.CHEESE),
+                new Ingredient("SLSA", "Salsa", Ingredient.Type.SAUCE),
+                new Ingredient("SRCR", "Sour Cream", Ingredient.Type.SAUCE)
         );
 
         Ingredient.Type[] types = Ingredient.Type.values();
--- src/main/java/com/example/tacos/model/Ingredient.java
+++ src/main/java/com/example/tacos/model/Ingredient.java
@@ -5,20 +5,18 @@ import lombok.RequiredArgsConstructor;
 
 // Lombok automatically generates equals(), hashCode(), toString(), ... at runtime
 
-@Data
 /*
 the @Data annotation at the class level is provided by Lombok and tells
 Lombok to generate all of those missing methods as well as a constructor that accepts
 all final properties as arguments.
  */
+@Data
 @RequiredArgsConstructor
 public class Ingredient {
-
-    private final int id;
+    private final String id;
     private final String name;
     private final Type type;
-
     public static enum Type {
-        WRAP, SAUCE, VEGGIES, CHEESE, PROTEIN
+        WRAP, PROTEIN, VEGGIES, CHEESE, SAUCE
     }
 }
