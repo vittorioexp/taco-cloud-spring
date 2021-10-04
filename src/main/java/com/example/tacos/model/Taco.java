43aac4e1b3b5aea843f23e07241889697c4acc56
Vittorio Esposito <esposito.vittorio@rocketmail.com>
2021-10-04 15:29:58 +0200

    add jpa annotations

--- src/main/java/com/example/tacos/model/Taco.java
+++ src/main/java/com/example/tacos/model/Taco.java
@@ -2,26 +2,32 @@ package com.example.tacos.model;
 
 import java.util.Date;
 import java.util.List;
+import javax.persistence.*;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
 import lombok.Data;
-import lombok.Getter;
-import lombok.Setter;
 
 @Data
-@Getter
-@Setter
+@Entity
 public class Taco {
 
+    @Id
+    @GeneratedValue(strategy= GenerationType.AUTO)
     private Long id;
 
     @NotNull
     @Size(min=5, message="Name must be at least 5 characters long")
     private String name;
 
+    @ManyToMany(targetEntity=Ingredient.class)
     @Size(min=1, message="You must choose at least 1 ingredient")
-    private List<String> ingredients;
+    private List<Ingredient> ingredients;
 
     private Date createdAt;
+
+    @PrePersist
+    void createdAt() {
+        this.createdAt = new Date();
+    }
 
 }
