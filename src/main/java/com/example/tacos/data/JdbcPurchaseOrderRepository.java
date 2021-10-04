f948895628dd7df252fa22dbcdb0a3c944288623
Vittorio Esposito <esposito.vittorio@rocketmail.com>
2021-10-04 09:09:41 +0200

    write jdbc purchase order repository

---
+++ src/main/java/com/example/tacos/data/JdbcPurchaseOrderRepository.java
@@ -0,0 +1,30 @@
+package com.example.tacos.data;
+
+import com.example.tacos.model.PurchaseOrder;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.simple.SimpleJdbcInsert;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class JdbcPurchaseOrderRepository implements PurchaseOrderRepository {
+    private SimpleJdbcInsert orderInserter;
+    private SimpleJdbcInsert orderTacoInserter;
+    private ObjectMapper objectMapper;
+
+    @Autowired
+    public JdbcPurchaseOrderRepository(JdbcTemplate jdbc) {
+        this.orderInserter = new SimpleJdbcInsert(jdbc)
+                .withTableName("Taco_Order")
+                .usingGeneratedKeyColumns("id");
+        this.orderTacoInserter = new SimpleJdbcInsert(jdbc)
+                .withTableName("Taco_Order_Tacos");
+        this.objectMapper = new ObjectMapper();
+    }
+
+    @Override
+    public PurchaseOrder save(PurchaseOrder order) {
+        return null;
+    }
+}
