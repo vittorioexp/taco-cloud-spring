dc7903e6342ddd2280fe461dd697c72aa600c3f2
Vittorio Esposito <esposito.vittorio@rocketmail.com>
2021-10-04 09:28:15 +0200

    fix jdbc purchase order repository

--- src/main/java/com/example/tacos/data/JdbcPurchaseOrderRepository.java
+++ src/main/java/com/example/tacos/data/JdbcPurchaseOrderRepository.java
@@ -1,11 +1,17 @@
 package com.example.tacos.data;
 
 import com.example.tacos.model.PurchaseOrder;
+import com.example.tacos.model.Taco;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.core.simple.SimpleJdbcInsert;
 import org.springframework.stereotype.Repository;
+
+import java.util.Date;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 @Repository
 public class JdbcPurchaseOrderRepository implements PurchaseOrderRepository {
@@ -25,6 +31,31 @@ public class JdbcPurchaseOrderRepository implements PurchaseOrderRepository {
 
     @Override
     public PurchaseOrder save(PurchaseOrder order) {
-        return null;
+        order.setCreatedAt(new Date());
+        long orderId = saveOrderDetails(order);
+        order.setId(orderId);
+        List<Taco> tacos = order.getTacos();
+        for (Taco taco : tacos) {
+            saveTacoToOrder(taco, orderId);
+        }
+        return order;
+    }
+
+    private long saveOrderDetails(PurchaseOrder order) {
+        @SuppressWarnings("unchecked")
+        Map<String, Object> values =
+                objectMapper.convertValue(order, Map.class);
+        values.put("createdAt", order.getCreatedAt());
+        long orderId =
+                orderInserter
+                        .executeAndReturnKey(values)
+                        .longValue();
+        return orderId;
+    }
+    private void saveTacoToOrder(Taco taco, long orderId) {
+        Map<String, Object> values = new HashMap<>();
+        values.put("tacoOrder", orderId);
+        values.put("taco", taco.getId());
+        orderTacoInserter.execute(values);
     }
 }
