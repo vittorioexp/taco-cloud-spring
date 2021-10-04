e1ae022c8434eafe5aa3c326eb9f25a476e4a3a9
Vittorio Esposito <esposito.vittorio@rocketmail.com>
2021-10-04 15:49:29 +0200

    add custom methods to purchase order repository

--- src/main/java/com/example/tacos/data/PurchaseOrderRepository.java
+++ src/main/java/com/example/tacos/data/PurchaseOrderRepository.java
@@ -3,5 +3,19 @@ package com.example.tacos.data;
 import com.example.tacos.model.PurchaseOrder;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.Date;
+import java.util.List;
+
 public interface PurchaseOrderRepository extends CrudRepository<PurchaseOrder, Long> {
+
+    List<PurchaseOrder> findByDeliveryZip(String deliveryZip);
+
+    List<PurchaseOrder> readOrdersByDeliveryZipAndPlacedAtBetween(
+            String deliveryZip, Date startDate, Date endDate);
+
+    List<PurchaseOrder> findByDeliveryNameAndDeliveryCity(
+            String deliveryTo, String deliveryCity);
+
+    List<PurchaseOrder> findByDeliveryCityOrderByDeliveryName(String city);
+
 }
