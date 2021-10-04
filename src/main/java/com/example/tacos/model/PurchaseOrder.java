b332aa008c8f8a728f13227c824d08035fa0087f
Vittorio Esposito <esposito.vittorio@rocketmail.com>
2021-10-04 15:42:47 +0200

    fix purchase order attributes

--- src/main/java/com/example/tacos/model/PurchaseOrder.java
+++ src/main/java/com/example/tacos/model/PurchaseOrder.java
@@ -24,22 +24,22 @@ public class PurchaseOrder {
     private Long id;
 
     @NotBlank(message="Name is required")
-    private String name;
+    private String deliveryName;
 
     @ManyToMany(targetEntity=Taco.class)
     private List<Taco> tacos = new ArrayList<>();
 
     @NotBlank(message="Street is required")
-    private String street;
+    private String deliveryStreet;
 
     @NotBlank(message="City is required")
-    private String city;
+    private String deliveryCity;
 
     @NotBlank(message="State is required")
-    private String state;
+    private String deliveryState;
 
     @NotBlank(message="Zip code is required")
-    private String zip;
+    private String deliveryZip;
 
     @CreditCardNumber(message="Not a valid credit card number")
     private String ccNumber;
