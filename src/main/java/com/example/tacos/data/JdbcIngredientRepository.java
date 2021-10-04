2be384412bc780014175cb347229f786b8d8ffb6
Vittorio Esposito <esposito.vittorio@rocketmail.com>
2021-10-04 08:17:01 +0200

    write JdbcIngredientRepository methods

--- src/main/java/com/example/tacos/data/JdbcIngredientRepository.java
+++ src/main/java/com/example/tacos/data/JdbcIngredientRepository.java
@@ -5,6 +5,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Repository;
 
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
 @Repository
 public class JdbcIngredientRepository implements IngredientRepository {
 
@@ -15,18 +18,33 @@ public class JdbcIngredientRepository implements IngredientRepository {
         this.jdbc = jdbc;
     }
 
+    private Ingredient mapRowToIngredient(ResultSet rs, int rowNum) throws SQLException {
+        return new Ingredient(
+                rs.getString("id"),
+                rs.getString("name"),
+                Ingredient.Type.valueOf(rs.getString("type")));
+    }
+
     @Override
     public Iterable<Ingredient> findAll() {
-        return null;
+        return jdbc.query("select id, name, type from Ingredient",
+                this::mapRowToIngredient);
     }
 
     @Override
     public Ingredient findOne(String id) {
-        return null;
+        return jdbc.queryForObject(
+                "select id, name, type from Ingredient where id=?",
+                this::mapRowToIngredient, id);
     }
 
     @Override
     public Ingredient save(Ingredient ingredient) {
-        return null;
+        jdbc.update(
+                "insert into Ingredient (id, name, type) values (?, ?, ?)",
+                ingredient.getId(),
+                ingredient.getName(),
+                ingredient.getType().toString());
+        return ingredient;
     }
 }
