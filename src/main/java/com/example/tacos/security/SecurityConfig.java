5c4e5563f3d985104947d1af0f6fbb0b4aebda2e
Vittorio Esposito <esposito.vittorio@rocketmail.com>
2021-10-06 11:49:06 +0200

    fix SecurityConfig (userDetailsService)

--- src/main/java/com/example/tacos/security/SecurityConfig.java
+++ src/main/java/com/example/tacos/security/SecurityConfig.java
@@ -6,28 +6,35 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-
-import javax.sql.DataSource;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Autowired
-    DataSource dataSource;
+    private UserDetailsService userDetailsService;
+
+    /*
+    configure a password encoder so that the password can be encoded in the database.
+    Because the encoder() method is annotated with @Bean, it will
+    be used to declare a PasswordEncoder bean in the Spring application context. Any
+    calls to encoder() will then be intercepted to return the bean instance from the
+    application context.
+     */
+    @Bean
+    public PasswordEncoder encoder() {
+        return new BCryptPasswordEncoder();
+    }
 
     @Override
     protected void configure(AuthenticationManagerBuilder auth)
             throws Exception {
         auth
-            .jdbcAuthentication()
-            .dataSource(dataSource)
-            .usersByUsernameQuery(
-                    "select username, password, enabled from Users " +
-                            "where username=?")
-            .authoritiesByUsernameQuery(
-                    "select username, authority from UserAuthorities " +
-                            "where username=?");
+            .userDetailsService(userDetailsService)
+            .passwordEncoder(encoder());
     }
 
 }
