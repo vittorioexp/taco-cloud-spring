e3c0257b3abb97ad6a731cd40313d97fafb9c963
Vittorio Esposito <esposito.vittorio@rocketmail.com>
2021-10-06 12:08:06 +0200

    add method "void configure (HttpSecurity)"" to SecurityConfig

--- src/main/java/com/example/tacos/security/SecurityConfig.java
+++ src/main/java/com/example/tacos/security/SecurityConfig.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.userdetails.UserDetailsService;
@@ -35,6 +36,19 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
         auth
             .userDetailsService(userDetailsService)
             .passwordEncoder(encoder());
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        String s1 = "/";
+        String s2 = "/**";
+
+        http
+            .authorizeRequests()
+            .antMatchers("/design", "/orders")
+            .hasRole("USER")
+            .antMatchers(s1, s2)
+            .permitAll();
     }
 
 }
