562c027c9f5228954fe84a6516c002db971e30fe
Vittorio Esposito <esposito.vittorio@rocketmail.com>
2021-10-06 11:08:29 +0200

    JDBC-based user store

--- src/main/java/com/example/tacos/security/SecurityConfig.java
+++ src/main/java/com/example/tacos/security/SecurityConfig.java
@@ -1,26 +1,26 @@
 package com.example.tacos.security;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
+import javax.sql.DataSource;
+
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
+    @Autowired
+    DataSource dataSource;
+
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
         auth
-            .inMemoryAuthentication()
-            .withUser("buzz")
-            .password("infinity")
-            .authorities("ROLE_USER")
-            .and()
-            .withUser("woody")
-            .password("bullseye")
-            .authorities("ROLE_USER");
+            .jdbcAuthentication()
+            .dataSource(dataSource);
     }
 
 }
