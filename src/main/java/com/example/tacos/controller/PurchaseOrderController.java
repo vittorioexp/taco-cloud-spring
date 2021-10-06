cbadb211aaf59f0b4f2307bd82f47fa249af4240
Vittorio Esposito <esposito.vittorio@rocketmail.com>
2021-10-06 12:49:01 +0200

    fix to set "user" attribute of PurchaseOrder

--- src/main/java/com/example/tacos/controller/PurchaseOrderController.java
+++ src/main/java/com/example/tacos/controller/PurchaseOrderController.java
@@ -4,6 +4,8 @@ import com.example.tacos.data.PurchaseOrderRepository;
 import com.example.tacos.model.*;
 
 import lombok.extern.slf4j.Slf4j;
+import org.apache.tomcat.util.net.openssl.ciphers.Authentication;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.Errors;
@@ -24,16 +26,20 @@ public class PurchaseOrderController {
     public PurchaseOrderController(PurchaseOrderRepository orderRepo) {
         this.orderRepo = orderRepo;
     }
+
     @GetMapping("/current")
     public String orderForm() {
         return "orderForm";
     }
+
     @PostMapping
     public String processOrder(@Valid PurchaseOrder order, Errors errors,
-                               SessionStatus sessionStatus) {
+                               SessionStatus sessionStatus,
+                               @AuthenticationPrincipal User user) {
         if (errors.hasErrors()) {
             return "orderForm";
         }
+        order.setUser(user);
         orderRepo.save(order);
         sessionStatus.setComplete();
         return "redirect:/";
