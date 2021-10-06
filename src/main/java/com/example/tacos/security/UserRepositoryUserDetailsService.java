f06fcce77aed542bdae4ab1e8290c07d913f54d3
Vittorio Esposito <esposito.vittorio@rocketmail.com>
2021-10-06 11:43:03 +0200

    write user repo service

---
+++ src/main/java/com/example/tacos/security/UserRepositoryUserDetailsService.java
@@ -0,0 +1,37 @@
+package com.example.tacos.security;
+
+import com.example.tacos.data.UserRepository;
+import com.example.tacos.model.User;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+
+/*
+@Service is another one of Spring’s stereotype annotations
+that flag it for inclusion in Spring’s component scanning,
+so there’s no need to explicitly declare this class as a bean.
+Spring will automatically discover it and instantiate it as a bean.
+ */
+@Service
+public class UserRepositoryUserDetailsService implements UserDetailsService {
+
+    private UserRepository userRepo;
+
+    @Autowired
+    public UserRepositoryUserDetailsService(UserRepository userRepo) {
+        this.userRepo = userRepo;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+
+        User user = userRepo.findByUsername(username);
+        if (user != null) {
+            return user;
+        }
+        throw new UsernameNotFoundException("User '" + username + "' not found");
+    }
+}
