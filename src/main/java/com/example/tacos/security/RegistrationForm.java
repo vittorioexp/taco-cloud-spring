5cec18ec4eea8efe9690cd722906779c9fcaf5a0
Vittorio Esposito <esposito.vittorio@rocketmail.com>
2021-10-06 11:55:26 +0200

    write RegistrationForm class

---
+++ src/main/java/com/example/tacos/security/RegistrationForm.java
@@ -0,0 +1,24 @@
+package com.example.tacos.security;
+
+import com.example.tacos.model.User;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import lombok.Data;
+
+@Data
+public class RegistrationForm {
+
+    private String username;
+    private String password;
+    private String fullname;
+    private String street;
+    private String city;
+    private String state;
+    private String zip;
+    private String phone;
+
+    public User toUser(PasswordEncoder passwordEncoder) {
+        return new User(
+                username, passwordEncoder.encode(password),
+                fullname, street, city, state, zip, phone);
+    }
+}
